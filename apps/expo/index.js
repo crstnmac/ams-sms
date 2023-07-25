d0f14d95d3282fa9444fcaf39a7daa99b785d4dd
crstnmac <criston2011@gmail.com>
2023-07-25 17:18:20 +0530

    feat(app): :sparkles: add clarity analytics

--- apps/expo/index.js
+++ apps/expo/index.js
@@ -1,4 +1,6 @@
 import 'expo-router/entry'
-
+import { initialize } from 'react-native-clarity';
 import { en, registerTranslation } from 'react-native-paper-dates'
+
 registerTranslation('en', en)
+initialize("i4sf7d3hth");
